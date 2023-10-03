805f124ef4aedcfaba0acf55bdd37580bb0f0576
maqa013 <125148102+maqa013@users.noreply.github.com>
2023-10-03 20:19:43 +0400

    Add files via upload

---
+++ jqanim.js
@@ -0,0 +1,212 @@
+// $('.animate').click(function(){
+//     $('.block').animate({
+//         // width:'+=100px',
+//         // left:'100px'
+//         // width:'toggle'
+
+       
+//     },1000)
+    
+// })
+
+
+// $('.first').mousemove(function(){
+//     // $('.first').add('second').css('background','red')
+//     // $('.first').addBack()('second')
+//     // $('.first').next().css('background','red')
+//     // $('.first').nextAll().css('background','red')
+
+//    let value = +$('h1').text() 
+//    $('h1').text(value += 1)
+
+
+// })
+
+// .one('click',function(){})
+// let value = $('.first').val()
+//     $('.first').mousmove(value + 1)
+  // $('.first').add('second').css('background','red')
+    // $('.first').addBack()('second')
+    // $('.first').next().css('background','red')
+    // $('.first').nextAll().css('background','red')
+
+
+
+
+
+
+    // 1)
+    // Создайте эффект слайдера используя slideDown для тега div при нажатии на тег button
+    // <div class="slide"></div>
+    // <button class="btn"></button>
+
+// $('.btn').click(function(){
+//   $('.slide').slideDown()
+// })
+
+    
+    // 2)
+    // Создайте эффект слайдера используя slideUp для тега div при нажатии на тег button
+    // <div class="slide"></div>
+    // <button class="btn"></button>
+
+
+    // $('.btn').click(function(){
+    //   $('.slide').slideUp()
+    // })
+
+    
+    // 3)
+    // Создайте эффект слайдера используя slideToggle для тега div при нажатии на тег button
+    // <div class="slide"></div>
+    // <button class="btn"></button>
+
+    // $('.btn').click(function(){
+    //   $('.slide').slideToggle()
+    // })
+
+
+    
+    // 4)
+    // Создайте эффект слайдера используя slideDown для тега div при нажатии на тег button и установите время анимации на 4s
+    // <div class="slide"></div>
+    // <button class="btn"></button>
+
+    // $('.btn').click(function(){
+    //   $('.slide').slideDown(4000)
+    // })
+
+    
+    // 5)
+    // Создайте эффект слайдера используя slideUp для тега div при нажатии на тег button и установите время анимации на 5s
+    // <div class="slide"></div>
+    // <button class="btn"></button>
+    
+    // $('.btn').click(function(){
+    //   $('.slide').slideUp(5000)
+    // })
+
+
+    // 6)
+    // Создайте эффект слайдера используя slideToggle для тега div при нажатии на тег button и установите время анимации на 6s
+    // <div class="slide"></div>
+    // <button class="btn"></button>
+
+    // $('.btn').click(function(){
+    //   $('.slide').slideToggle(6000)
+    // })
+
+
+    
+    // 7)
+    // Создайте эффект слайдера используя slideUp для тега div при нажатии на тег button, установите время анимации на 5s и в конце анимации выведите в alert сообщение "Вверх"
+    // <div class="slide"></div>
+    // <button class="btn"></button>
+   
+
+    
+//     $('.animate').click(function(){
+//       $('div').slideUp(5000,function alert(){
+// alert('Up')
+//       })
+//     }
+
+// )
+
+
+
+
+
+    
+    // 8)
+    // Создайте эффект слайдера используя slideDown для тега div при нажатии на тег button, установите время анимации на 5s и в конце анимации выведите в alert сообщение "Вниз"
+    // <div class="slide"></div>
+    // <button class="btn"></button>
+
+
+
+
+    // $('.animate').click(function(){
+    //   $('div').slideDown(5000,
+    //     function alert(){
+    //       alert('down')
+    //     })
+    // })
+
+
+    
+    // 9)
+    // Создайте эффект слайдера используя slideToggle для тега div при нажатии на тег button, установите время анимации на 5s и в конце анимации выведите в alert сообщение "Движение"
+    // <div class="slide"></div>
+    // <button class="btn"></button>
+    
+ // $('.animate').click(function(){
+    //   $('div').slideToggle(5000,
+    //     function alert(){
+    //       alert('move')
+    //     })
+    // })
+   
+
+    // $('.animate').click(function(alert){
+    //   $('div').slideToggle(1000, 
+    //     function alert(){
+    //     alert('up')
+    //   })
+    // })
+
+
+
+    // 10)
+    // Создайте эффект движения в лево для тега div, при нажатии на button с помощью метода animate
+    // <div class="slide"></div>
+    // <button class="btn"></button>
+
+   // $('.btn').click(function(){
+    //   $('.slide').animate({
+// height:"something px",
+// widt:"something px"
+    // })
+    // })
+
+
+    
+    // 11)
+    // Создайте эффект увеличения высоты и ширины для тега div, при нажатии на button с помощью метода animate
+    // <div class="slide"></div>
+    // <button class="btn"></button>
+    
+// $('.btn').click(function(){
+    //   $('.slide').animate({
+// left:'something px'
+    // })
+    // })
+
+    // 12)
+    // Для чего используется метод .stop()?
+    // для остановки анимации до ее завершения
+    // 13)
+    // Какие 2 параметра принимает в себя .stop() метод?
+    // stopAll,goToEnd
+    // 14)
+    // Для чего мы используем метод .css({})?
+    // чтобы задать стили элементу через JQ
+    
+    // 15)
+    // Является ли скрипт ниже ошибочным?
+    $("#p1").css("color", "red").slideUp(2000).slideDown(2000)
+
+
+    // Нет
+
+
+
+
+
+
+
+
+
+
+
+
